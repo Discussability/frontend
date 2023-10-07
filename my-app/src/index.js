50e49c017914d35ab55ed5f9478270e58c31c2ec
Angela Yang <angela@MacBook-Air-2.local>
2023-10-06 18:04:16 -0700

    added other webpages

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -15,6 +15,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
 let text = document.getElementById('text');
 let text2 = document.getElementById('text2');
 let back1 = document.getElementById('back1');
